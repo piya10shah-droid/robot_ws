c707f1a2d48b8d4c799016cbff865274d6bf2145
shourya <pihushourya100@gmail.com>
2026-05-26 21:45:05 +0530

    removed dependency from cmake of bumperbot_planning pkg, added PathSubstitution in slam launch, added indent slam_toolbox.yaml

--- src/bumperbot_mapping/launch/slam.launch.py
+++ src/bumperbot_mapping/launch/slam.launch.py
@@ -3,7 +3,7 @@ from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 
 
 def generate_launch_description():
