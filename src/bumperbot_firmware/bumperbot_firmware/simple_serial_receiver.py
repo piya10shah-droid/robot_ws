524083a9eba8267374963dcf8617325948f05d1c
shourya <pihushourya100@gmail.com>
2026-06-23 16:20:31 +0530

    remove counter from simple_serial_transmitter.py

--- src/bumperbot_firmware/bumperbot_firmware/simple_serial_receiver.py
+++ src/bumperbot_firmware/bumperbot_firmware/simple_serial_receiver.py
@@ -1,49 +1,40 @@
 #!/usr/bin/env python3
-import serial
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+import serial
 
-
-class SimpleSerialReceiver(Node):
+class SimpleSerialTransmitter(Node):
     def __init__(self):
-        super().__init__("simple_serial_receiver")
+        super().__init__("simple_serial_transmitter")
 
-        self.declare_parameter("port", "/dev/ttyUSB0")
+        self.declare_parameter("port", "/dev/arduino")
         self.declare_parameter("baudrate", 115200)
 
-        self.port_ = self.get_parameter("port").value
-        self.baudrate_ = self.get_parameter("baudrate").value
+        port = self.get_parameter("port").value
+        baudrate = self.get_parameter("baudrate").value
 
-        self.pub_ = self.create_publisher(String, "serial_receiver", 10)
-        self.arduino_ = serial.Serial(port=self.port_, baudrate=self.baudrate_, timeout=0.1)
+        self.arduino = serial.Serial(port=port, baudrate=baudrate, timeout=0.1)
+        
+        # Subscribe to the topic
+        self.subscription = self.create_subscription(
+            String,
+            '/serial_transmitter',
+            self.listener_callback,
+            10
+        )
 
-        self.frequency_ = 0.01
-        self.timer_ = self.create_timer(self.frequency_, self.timerCallback)
-
-    def timerCallback(self):
-        if rclpy.ok() and self.arduino_.is_open:
-            data = self.arduino_.readline()
-
-            try:
-                data.decode("utf-8")
-            except:
-                return
-
-            msg = String()
-            msg.data = str(data)
-            self.pub_.publish(msg)
-
+    def listener_callback(self, msg):
+        # Send the received string data directly to serial
+        self.arduino.write(msg.data.encode("utf-8"))
+        self.get_logger().info(f"Forwarded: {msg.data}")
 
 def main():
     rclpy.init()
-
-    simple_serial_receiver = SimpleSerialReceiver()
-    rclpy.spin(simple_serial_receiver)
-    
-    simple_serial_receiver.destroy_node()
+    node = SimpleSerialTransmitter()
+    rclpy.spin(node)
+    node.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
