909dc80cc9dc1274b7b5ecca5de37f5e880363a9
Jetson <pihushourya100@gmail.com>
2026-06-26 17:19:50 +0530

    add comment to simulated_robot.launch.py for testing git on jetson

--- src/bumperbot_bringup/launch/simulated_robot.launch.py
+++ src/bumperbot_bringup/launch/simulated_robot.launch.py
@@ -6,6 +6,7 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
+#testing
 
 
 def generate_launch_description():
