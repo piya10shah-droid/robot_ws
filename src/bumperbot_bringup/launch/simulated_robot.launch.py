a0d1691d8708ca01acc65069fd6f9d5c742fd91b
Jetson <pihushourya100@gmail.com>
2026-06-26 17:20:35 +0530

    remove comment as testing is done

--- src/bumperbot_bringup/launch/simulated_robot.launch.py
+++ src/bumperbot_bringup/launch/simulated_robot.launch.py
@@ -6,8 +6,6 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
-#testing
-
 
 def generate_launch_description():
     use_slam = LaunchConfiguration("use_slam")
